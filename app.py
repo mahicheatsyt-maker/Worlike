2fdc824d3b379845f8b8cbae2c48495255976f61
mahicheatsyt-maker <mahicheatsyt@gmail.com>
2026-04-09 20:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from google.protobuf.message import DecodeError
 app = Flask(__name__)
 
 def load_tokens(server_name):
-    REPO = "mahicheatsyt-maker/Tokens/tree/main"
+    REPO = "mahicheatsyt-maker/Tokens"
     BASE_URL = f"https://raw.githubusercontent.com/{REPO}/main/"
     
     try:
